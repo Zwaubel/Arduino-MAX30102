7e3d7711177a647a0ae1b763be9eb37a5f06407f
Vincent Abt <dervinci@web.de>
2021-10-10 17:47:56 +0200

    improved test settings for a finger ppg

--- examples/MAX30102_RawData/MAX30102_RawData.ino
+++ examples/MAX30102_RawData/MAX30102_RawData.ino
@@ -1,22 +1,22 @@
 /*
-Arduino-MAX30102 oximetry / heart rate integrated sensor library by Shivam Gupta (gupta.shivam1996@gmail.com)
+  Arduino-MAX30102 oximetry / heart rate integrated sensor library by Shivam Gupta (gupta.shivam1996@gmail.com)
 
-Based on MAX30100 library, Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
-All alogrithms and methods used are from the above author,
-I have only modified this enough to make it work with the new MAX30102 sensor.
+  Based on MAX30100 library, Copyright (C) 2016  OXullo Intersecans <x@brainrapers.org>
+  All alogrithms and methods used are from the above author,
+  I have only modified this enough to make it work with the new MAX30102 sensor.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // The example shows how to retrieve raw values from the sensor
@@ -28,12 +28,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Sampling is tightly related to the dynamic range of the ADC.
 // refer to the datasheet for further info
-#define SAMPLING_RATE                       MAX30102_SAMPRATE_100HZ
+#define SAMPLING_RATE                       MAX30102_SAMPRATE_400HZ
 
 // The LEDs currents must be set to a level that avoids clipping and maximises the
 // dynamic range
-#define IR_LED_CURRENT                      0xff
-#define RED_LED_CURRENT                     0x88
+#define IR_LED_CURRENT                      0x16
+#define RED_LED_CURRENT                     0x1b
 
 // The pulse width of the LEDs driving determines the resolution of
 // the ADC (which is a Sigma-Delta).
@@ -44,42 +44,39 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Instantiate a MAX30102 sensor class
 MAX30102 sensor;
+uint32_t ir, red;
 
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    Serial.print("Initializing MAX30102..");
+  // Initialize the sensor
+  // Failures are generally due to an improper I2C wiring, missing power supply
+  // or wrong target chip
+  if (!sensor.begin()) {
+    Serial.println("MAX30102 INIT FAILED");
+    while (true);
+  }
+  Serial.println("IR\tRed");
 
-    // Initialize the sensor
-    // Failures are generally due to an improper I2C wiring, missing power supply
-    // or wrong target chip
-    if (!sensor.begin()) {
-        Serial.println("FAILED");
-        for(;;);
-    } else {
-        Serial.println("SUCCESS");
-    }
-
-    // Set up the wanted parameters
-    sensor.setMode(MAX30102_MODE_SPO2_HR);
-    //sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT);
-    sensor.setIRLedCurrent(IR_LED_CURRENT);
-    sensor.setRedLedCurrent(RED_LED_CURRENT);
-    sensor.setLedsPulseWidth(PULSE_WIDTH);
-    sensor.setSamplingRate(SAMPLING_RATE);
-    sensor.setRangeADC(MAX30102_ADCRange_16384);
+  // Set up the wanted parameters
+  sensor.setMode(MAX30102_MODE_SPO2_HR);
+  //sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT);
+  sensor.setIRLedCurrent(IR_LED_CURRENT);
+  sensor.setRedLedCurrent(RED_LED_CURRENT);
+  sensor.setLedsPulseWidth(PULSE_WIDTH);
+  sensor.setSamplingRate(SAMPLING_RATE);
+  sensor.setSampleAveraging(MAX30102_SMP_AVE_16);
+  sensor.setRangeADC(MAX30102_ADCRange_16384);
 }
 
 void loop()
 {
-    uint16_t ir, red;
+  sensor.update();
 
-    sensor.update();
-
-    while (sensor.getRawValues(&ir, &red)) {
-        Serial.print(ir);
-        Serial.print('\t');
-        Serial.println(red);
-    }
+  while (sensor.getRawValues(&ir, &red)) {
+    Serial.print(ir);
+    Serial.print('\t');
+    Serial.println(red);
+  }
 }
